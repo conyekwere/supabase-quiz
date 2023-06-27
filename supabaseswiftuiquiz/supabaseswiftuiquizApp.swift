96cbcbd2215544c572d174a28a70cfe775b5bd42
Your Name <you@example.com>
2023-06-26 22:37:42 -0700

    Initial Commit

---
+++ supabaseswiftuiquiz/supabaseswiftuiquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  supabaseswiftuiquizApp.swift
+//  supabaseswiftuiquiz
+//
+//  Created by Chima onyekwere on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct supabaseswiftuiquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
